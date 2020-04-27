6217dd9113e10e7453e3f3d777bb32a138b62671
Rhodri Nelson <rhodri.nelson@gmail.com>
2020-04-27 17:33:08 +0100

    trim ex.

--- examples/seismic/skew_self_adjoint/exampleIso.py
+++ examples/seismic/skew_self_adjoint/exampleIso.py
@@ -4,14 +4,7 @@ from devito import configuration
 from examples.seismic.skew_self_adjoint import *
 configuration['language'] = 'openmp'
 configuration['log-level'] = 'DEBUG'
-# configuration['autotuning'] = 'aggressive'
-# configuration['mpi'] = 1
-# configuration['mpi'] = 'full'
 
-# os.environ["OMP_PLACES"] = 'cores'
-# os.environ["OMP_PROC_BIND"] = 'spread'
-
-# shape = (600, 600)
 shape = (600, 600, 600)
 dtype = np.float32
 npad = 10
@@ -22,10 +15,5 @@ fpeak = 0.010
 omega = 2.0 * np.pi * fpeak
 
 v, b, time_axis, src, rec = defaultSetupIso(npad, shape, dtype, tmax=tmax)
-print(time_axis)
-# solver = SSA_ISO_AcousticWaveSolver(npad, qmin, qmax, omega, b, v, src, rec, time_axis, space_order=4)
 solver = SSA_ISO_AcousticWaveSolver(npad, qmin, qmax, omega, b, v, src, rec, time_axis, space_order=8)
-# solver = SSA_ISO_AcousticWaveSolver(npad, qmin, qmax, omega, b, v, src, rec, time_axis, space_order=16)
-
 rec, u, summary = solver.forward()
-# rec, u, summary = solver.forward(autotune=('aggressive', 'runtime'))
